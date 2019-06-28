8500c66b0959031d23f8bf1972334df209dfbeed
abdul <abdul.tanveer@gmail.com>
2019-06-29 05:08:50 +0530

    intents and passing data

--- app/src/main/java/com/example/abdul/MainActivity.java
+++ app/src/main/java/com/example/abdul/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.abdul;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    public  static String KEY = "name";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,6 +16,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void handleClick(View view) {
-        Toast.makeText(this, "welcome", Toast.LENGTH_SHORT).show();
+       // Toast.makeText(this, "welcome", Toast.LENGTH_SHORT).show();
+        EditText nameEditText = findViewById(R.id.editTextName); //took handle on the edittext
+        String name = nameEditText.getText().toString();        //got the string
+        Intent homeIntent = new Intent(MainActivity.this,HomeActivity.class);
+        homeIntent.putExtra(KEY,name);                          //inserted that string into intent
+        startActivity(homeIntent);
     }
 }
