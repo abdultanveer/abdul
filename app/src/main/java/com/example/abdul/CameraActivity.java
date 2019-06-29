a7327eb7cdcfaebff7fb680897cbd0eda750bd83
abdul <abdul.tanveer@gmail.com>
2019-06-29 07:31:45 +0530

    log -- logcat

--- app/src/main/java/com/example/abdul/CameraActivity.java
+++ app/src/main/java/com/example/abdul/CameraActivity.java
@@ -3,16 +3,20 @@ package com.example.abdul;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class CameraActivity extends AppCompatActivity {
     public static  String PHOTO = "photo";
+    public  static  String TAG = CameraActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Log.e(TAG,"oncreate");
 
       String name =  getIntent().getExtras().getString(WhatsappActivity.KEY);
         TextView nameTextView = findViewById(R.id.textViewName);
@@ -27,6 +31,7 @@ public class CameraActivity extends AppCompatActivity {
 
         Intent resultIntent = new Intent();
         resultIntent.putExtra(PHOTO, pixels);
+        Log.w(TAG,"clickHandler");
 
         setResult(RESULT_OK,resultIntent); //result_ok indicates the parentactivity that result is consistent
         finish(); //this will stop/finish the childactivity[Camera activity]
--- app/src/main/java/com/example/abdul/WhatsappActivity.java
+++ app/src/main/java/com/example/abdul/WhatsappActivity.java
@@ -1,16 +1,19 @@
 package com.example.abdul;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class WhatsappActivity extends AppCompatActivity {
+public class WhatsappActivity extends AppCompatActivity { //AppCompatActivity will ensure  backward compatibility
     public  static String KEY = "name";
+    public  static  String TAG = WhatsappActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,13 +23,15 @@ public class WhatsappActivity extends AppCompatActivity {
     public void handleClick(View view) {
         switch (view.getId()){
             case R.id.buttonLogin:
+                Log.i(TAG,"login button was clicked");
                 EditText nameEditText = findViewById(R.id.editTextName); //took handle on the edittext
                 String name = nameEditText.getText().toString();        //got the string
-                Intent homeIntent = new Intent(WhatsappActivity.this, CameraActivity.class);
+                Intent homeIntent = new Intent(WhatsappActivity.this, CameraActivity.class);  //explicit intent
                 homeIntent.putExtra(KEY,name);                          //inserted that string into intent
                 startActivityForResult(homeIntent,007); //intent, request code
                 break;
             case R.id.buttonCancel:
+                //implicit intent
                 Intent dialIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:123456789"));
                 startActivity(dialIntent);
                 break;
@@ -42,6 +47,8 @@ public class WhatsappActivity extends AppCompatActivity {
         if(resultCode == RESULT_OK && requestCode == 007) {
             String photo = data.getExtras().getString(CameraActivity.PHOTO);
             Toast.makeText(this, "photo", Toast.LENGTH_LONG).show();
+            Log.d(TAG,"onActivityResult--"+photo);//TAG = whatsappactivity
+
         }
         //request -- identify which activity is returning i.e camera,contact, location
     }
