659059dbd210d440039748cdaef38db1dff52100
abdul <abdul.tanveer@gmail.com>
2019-07-01 05:13:24 +0530

    lifecycle of an activity and savedinstance state

--- app/src/main/java/com/example/abdul/WhatsappActivity.java
+++ app/src/main/java/com/example/abdul/WhatsappActivity.java
@@ -3,21 +3,57 @@ package com.example.abdul;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class WhatsappActivity extends AppCompatActivity { //AppCompatActivity will ensure  backward compatibility
     public  static String KEY = "name";
+    public  static String MY_KEY = "mykey";
+
     public  static  String TAG = WhatsappActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(savedInstanceState !=null) {
+            Log.i(TAG, "mysaveddata= " + savedInstanceState.getString(MY_KEY));
+        }
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG,"onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG,"onResume");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG,"onPause");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG,"onStop");
+
     }
 
     public void handleClick(View view) {
@@ -40,6 +76,12 @@ public class WhatsappActivity extends AppCompatActivity { //AppCompatActivity wi
        // Toast.makeText(this, "welcome", Toast.LENGTH_SHORT).show();
     }
 
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        outState.putString(MY_KEY,"saved data");
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
